15471cff638da599dc57d73747d49c173a5b2955
user.email <sawepeter6@gmail.com>
2018-03-12 21:22:44 +0300

    hero-detail creation

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dev Kim'},
+    { id: 12, name: 'Dev Ngeno'},
+    { id: 13, name: 'Dev nixon'},
+    { id: 14, name: 'Dev Ashueh'},
+    { id: 15, name: 'Dev Githiomi'},
+    { id: 16, name: 'Dev Ireri'},
+    { id: 17, name: 'Dev Kal'},
+    { id: 18, name: 'Dev Orenge'},
+    { id: 19, name: 'Dev Pundo'},
+    { id: 20, name: 'Dev Okinyi'},
+]
