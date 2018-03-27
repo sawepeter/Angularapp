0115dd603d65e4448eee148d465b448b4172b086
user.email <sawepeter6@gmail.com>
2018-03-27 19:13:06 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes } from '@angular/router';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+
+const routes: Routes = [
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'detail/:id', component: HeroDetailComponent },
+    {path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
+imports: [RouterModule.forRoot(routes)],
  exports: [ RouterModule]
 })
 export class AppRoutingModule { }
