436a8d81275b10ad524a0dc10472c19402a105fd
user.email <sawepeter6@gmail.com>
2018-03-27 19:02:53 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes } from '@angular/router';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
  exports: [ RouterModule]
